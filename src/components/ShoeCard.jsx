1b6b38d4d737597da33d4943d25f09a6b68c0be1
Andrew Liu <ahliu1@gmail.com>
2023-10-28 14:15:53 -0700

    hero update

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -7,17 +7,21 @@ const ShoeCard = ({ imgUrl, changeBigShoeImage, bigShoeImg }) => {
 	return (
 		<div
 			className={`border-2 rounded-xl ${
-				bigShoeImg === imgUrl ? "border-coral-red" : "border-transparent"
+				bigShoeImg === imgUrl.bigShoe
+					? "border-coral-red"
+					: "border-transparent"
 			} cursor-pointer`}
 			onClick={handleClick}
 		>
-			<img
-				src={imgUrl.thumbnail}
-				alt="shoe collection"
-				width={127}
-				height={103}
-				className="object-contain"
-			/>
+			<div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+				<img
+					src={imgUrl.thumbnail}
+					alt="shoe collection"
+					width={127}
+					height={103}
+					className="object-contain"
+				/>
+			</div>
 		</div>
 	);
 };
